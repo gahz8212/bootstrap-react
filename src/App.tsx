a894e233aa7c464a6eca29b3ecb84d858080bbe8
sunghuyn Kim <berserk.gahz@gmail.com>
2025-03-28 21:54:19 +0900

    scss 설정과 Grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,23 @@
-import Badge from 'react-bootstrap/Badge';
-import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './App.scss';
+
 function App() {
 
 
   return (
-    <div>
-      <h1>
-        Example heading
-        <Badge bg="secondary" as={Button} >
-          New
-        </Badge>
-      </h1>
-    </div>
+    <Container>
+      <Row>
+        <Col sm={8}>sm=8</Col>
+        <Col sm={4}>sm=4</Col>
+      </Row>
+      <Row>
+        <Col sm>sm=true</Col>
+        <Col sm>sm=true</Col>
+        <Col sm>sm=true</Col>
+      </Row>
+    </Container>
   );
 
 }
